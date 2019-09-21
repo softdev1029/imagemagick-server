5c8ec18ec085019b4d6e9fdbb4d492fc284ef9cd
softdev1029 <softdev1029@outlook.com>
2019-09-21 17:28:13 +0800

    move file

--- upload.js
+++ upload.js
@@ -1,5 +1,21 @@
 const IncomingForm = require("formidable").IncomingForm;
 
+//moves the $file to $dir2
+var moveFile = (file, dir2)=>{
+  //include the fs, path modules
+  var fs = require('fs');
+  var path = require('path');
+
+  //gets file name and adds it to dir2
+  var f = path.basename(file);
+  var dest = path.resolve(dir2, f);
+
+  fs.rename(file, dest, (err)=>{
+    if(err) throw err;
+    else console.log('Successfully moved');
+  });
+};
+
 module.exports = function upload(req, res) {
   var form = new IncomingForm();
 
@@ -8,6 +24,7 @@ module.exports = function upload(req, res) {
     // e.g. save it to the database
     // you can access it using file.path
     console.log(file);
+    moveFile(file.path, '/var/www/html/PatternTile2/InputPatterns');
   });
   form.on("end", () => {
     res.json();
